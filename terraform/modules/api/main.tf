83c0bcc907b5e8ef2631772593d16a854d758745
ifaakash <aakashc.appregis@gmail.com>
2025-06-22 13:47:14 +0530

    chore(api): Add sub endpoint for REST API

--- terraform/modules/api/main.tf
+++ terraform/modules/api/main.tf
@@ -1,33 +1,33 @@
 resource "aws_api_gateway_rest_api" "api" {
-  name = "whats-my-RESTAPI"
+  name        = "whats-my-RESTAPI"
   description = "REST API for lambda gateway"
 }
 
-resource "aws_api_gateway_resource" "link"{
-  parent_id = aws_api_gateway_rest_api.api.root_resource_id
-  path_part =
+resource "aws_api_gateway_resource" "link" {
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "myip"
   rest_api_id = aws_api_gateway_rest_api.api.id
 }
 
-resource "aws_api_gateway_method" "method"{
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.link.id
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.link.id
   authorization = "NONE"
-  http_method = "GET"
+  http_method   = "GET"
 }
 
-resource "aws_api_gateway_integration" "integration"{
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.link.id
-  http_method = "GET"
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.link.id
+  http_method             = "GET"
   integration_http_method = "POST"
-  type = "AWS_PROXY"
+  type                    = "AWS_PROXY"
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   triggers = {
-    redeployment= sha1(jsonencode(
+    redeployment = sha1(jsonencode(
       [
         aws_api_gateway_integration.integration.id,
         aws_api_gateway_method.method.id
@@ -39,8 +39,8 @@ resource "aws_api_gateway_deployment" "deployment" {
   }
 }
 
-resource "aws_api_gateway_stage" "stage"{
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name = "dev"
+resource "aws_api_gateway_stage" "stage" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  stage_name    = "dev"
   deployment_id = aws_api_gateway_deployment.deployment.id
 }
